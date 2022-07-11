bc5c429f781280878717a67c3630457f344a4675
Ori Bibas <oribibas6@gmail.com>
2022-07-11 01:06:55 -0400

    gui test

--- src/test/java/GUI/GUITest.java
+++ src/test/java/GUI/GUITest.java
@@ -1,7 +1,19 @@
 package GUI;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GUITest {
+
+    GUI gui = new GUI();
+
+    @Test
+    public void testFrameProperties() {
+        Assertions.assertNotNull(gui.frame);
+        Assertions.assertTrue(gui.frame.getHeight() == 800);
+        Assertions.assertTrue(gui.frame.getWidth() == 780);
+    }
 
 }
---
+++ src/test/java/Methods/getUtilityTest.java
@@ -0,0 +1,11 @@
+package Methods;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class getUtilityTest {
+
+    getUtility util = new getUtility();
+
+}
